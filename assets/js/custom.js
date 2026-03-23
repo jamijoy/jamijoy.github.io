f599c882a51c150aa6832fe0d912d4ae7a988267
jamijoy <jamijoyy@gmail.com>
2026-03-24 00:40:13 +0600

    feat: update file paths in HTML and JavaScript for consistency and improved accessibility

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,16 @@
-fetch('components/footer.html')
+fetch('/components/footer.html')
 .then(response => response.text())
 .then(data => {
   document.getElementById('footer').innerHTML = data;
 });
 
-fetch('components/navbar.html')
+fetch('/components/navbar.html')
 .then(response => response.text())
 .then(data => {
   document.getElementById('navbar').innerHTML = data;
 });
 
-fetch('components/header.html')
+fetch('/components/header.html')
 .then(response => response.text())
 .then(data => {
   document.getElementById('header').innerHTML = data;
