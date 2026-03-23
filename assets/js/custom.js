c77f2dbc3c3ef127ad499aafcf516089ec890c49
jamijoy <jamijoyy@gmail.com>
2026-03-23 21:16:53 +0600

    feat: refactor website structure by modularizing header, footer, and navbar components; update hero section title for clarity

---
+++ assets/js/custom.js
@@ -0,0 +1,17 @@
+fetch('components/footer.html')
+.then(response => response.text())
+.then(data => {
+  document.getElementById('footer').innerHTML = data;
+});
+
+fetch('components/navbar.html')
+.then(response => response.text())
+.then(data => {
+  document.getElementById('navbar').innerHTML = data;
+});
+
+fetch('components/header.html')
+.then(response => response.text())
+.then(data => {
+  document.getElementById('header').innerHTML = data;
+});
