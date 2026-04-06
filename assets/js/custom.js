a69c78171e1b61fc91383e0b6d8693e4b0f5c1d3
jamijoy <jamijoyy@gmail.com>
2026-04-07 01:40:14 +0600

    fix: update contact form submission to use JSON format for data

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -16,18 +16,27 @@ fetch('/components/header.html')
   document.getElementById('header').innerHTML = data;
 });
 
+
 $('#contact-msg-save-form').submit(function(e) {
-  alert('Form submitted!');
   e.preventDefault();
-  var formData = $(this).serialize();
+
+  var formArray = $(this).serializeArray();
+  var formData = {};
+
+  formArray.forEach(function(field) {
+    formData[field.name] = field.value;
+  });
+
   $.ajax({
     type: 'POST',
     url: 'https://sms-service-scheduler-program.onrender.com/save-contact-form',
-    data: formData,
+    data: JSON.stringify(formData),
     contentType: "application/json",
-    processData: false,
     success: function(response) {
       alert('Message sent successfully!');
+    },
+    error: function(err) {
+      console.error(err);
     }
   });
 });
