3b4c91c5c82eed97a969f2bbf488b835a118cbd5
jamijoy <jamijoyy@gmail.com>
2026-05-21 08:56:53 +0600

    feat: implement page loader with spinner for improved loading experience

--- assets/js/main.js
+++ assets/js/main.js
@@ -211,3 +211,11 @@
   });
 
 })()
+
+// LOADER STOPPER
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.getElementById("loader").style.display = "none";
+  }, 500);
+});
